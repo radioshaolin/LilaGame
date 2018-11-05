b6cd27172e4f0f7381072add802ea3d7bc29941c
Ivan Zemlyaniy <ivan.zemlyaniy@x2sy.com>
2018-11-05 14:40:46 +0200

    WIP

--- LilaGame/Flows/MainTabbarFlow/TabbarCoordinator.swift
+++ LilaGame/Flows/MainTabbarFlow/TabbarCoordinator.swift
@@ -7,64 +7,64 @@
 //
 
 import UIKit
-//
-//
-//class TabbarCoordinator: Coordinator<DeepLink>, UITabBarControllerDelegate {
-//    
-//    private let tabbarView: TabbarView
-//    private let coordinatorFactory: CoordinatorFactory
-//    
-//    init(tabbarView: TabbarView, coordinatorFactory: CoordinatorFactory) {
-//        self.tabbarView = tabbarView
-//        self.coordinatorFactory = coordinatorFactory
-//    }
-//    
-//    override func start() {
-//        tabbarView.onViewDidLoad = runJoinGameFlow()
-//        tabbarView.onJoinGameFlowSelect = runJoinGameFlow()
-//        tabbarView.onSettingsFlowSelect = runSettingsFlow()
-//    }
-//    
-//    private func runJoinGameFlow() -> ((UINavigationController) -> ()) {
-//        return { navigationController in
-//            guard navigationController.viewControllers.isEmpty else { return }
-//            let (joinGameCoordinator, router) = self.coordinatorFactory.makeStartGameCoordinator(router: Router(navigationController: navigationController))
-//            self.addChild(joinGameCoordinator)
-//            joinGameCoordinator.start()
-//        }
-//    }
-//    
-//    private func runSettingsFlow() -> ((UINavigationController) -> ()) {
-//        return { navController in
-//            if navController.viewControllers.isEmpty == true {
-//                let settingsCoordinator = self.coordinatorFactory.makeSettingsCoordinator(navController: navController)
-//                settingsCoordinator.start()
-//                self.addDependency(settingsCoordinator)
-//            }
-//        }
-//    }
-//    
-//    
-//    
-//    // MARK: UITabBarControllerDelegate
-//    
-//    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
-//        //    guard let coordinator = tabs[viewController] else { return true }
-//        //
-//        //    // Let's protect this tab because we can
-//        //    if coordinator is AccountCoordinator && !store.isLoggedIn {
-//        //        presentAuthFlow()
-//        //        return false
-//        //    } else {
-//        //        return true
-//        //    }
-//    }
-//    
-//    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
-//        
-//    }
-//    
-//}
+
+
+class TabbarCoordinator: Coordinator<DeepLink>, UITabBarControllerDelegate {
+    
+    private let tabbarView: TabbarView
+    private let coordinatorFactory: CoordinatorFactory
+    
+    init(tabbarView: TabbarView, coordinatorFactory: CoordinatorFactory) {
+        self.tabbarView = tabbarView
+        self.coordinatorFactory = coordinatorFactory
+    }
+    
+    override func start() {
+        tabbarView.onViewDidLoad = runJoinGameFlow()
+        tabbarView.onJoinGameFlowSelect = runJoinGameFlow()
+        tabbarView.onSettingsFlowSelect = runSettingsFlow()
+    }
+    
+    private func runJoinGameFlow() -> ((UINavigationController) -> ()) {
+        return { navigationController in
+            guard navigationController.viewControllers.isEmpty else { return }
+            let (joinGameCoordinator, router) = self.coordinatorFactory.makeGameCoordinator()
+            self.addChild(joinGameCoordinator)
+            joinGameCoordinator.start()
+        }
+    }
+    
+    private func runSettingsFlow() -> ((UINavigationController) -> ()) {
+        return { navController in
+            if navController.viewControllers.isEmpty == true {
+                let settingsCoordinator = self.coordinatorFactory.makeSettingsCoordinator(navController: navController)
+                settingsCoordinator.start()
+                self.addDependency(settingsCoordinator)
+            }
+        }
+    }
+    
+    
+    
+    // MARK: UITabBarControllerDelegate
+    
+    func tabBarController(_ tabBarController: UITabBarController, shouldSelect viewController: UIViewController) -> Bool {
+        //    guard let coordinator = tabs[viewController] else { return true }
+        //
+        //    // Let's protect this tab because we can
+        //    if coordinator is AccountCoordinator && !store.isLoggedIn {
+        //        presentAuthFlow()
+        //        return false
+        //    } else {
+        //        return true
+        //    }
+    }
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        
+    }
+    
+}
 
 
 //class MainCoordinator: Coordinator<DeepLink> {
